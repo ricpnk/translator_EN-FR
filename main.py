8135127ebce3f56e2baeeda5d4e1100112d60415
ricopenkalski <51368644+ricopenkalski@users.noreply.github.com>
2025-05-19 01:28:00 +0200

    added fr vocab

--- main.py
+++ main.py
@@ -4,17 +4,27 @@ from src.Vocab import Vocab
 from collections import Counter
 
 def main():
+    #todo read in the data
     data = pd.read_csv("data/rec05_small_en_fr.csv")
 
-
+    #todo initialize counters for the vocabs
     en_counter = Counter()
+    fr_counter = Counter()
     for sentence in data["EN"]:
         en_counter.update(sentence.split())
+    for sentence in data["FR"]:
+        fr_counter.update(sentence.split())
 
-    vocabulary = Vocab()
-    vocabulary.build(en_counter)
+    #todo create vocabs for input and output
+    vocabulary_en = Vocab()
+    vocabulary_fr = Vocab()
+    vocabulary_en.build(en_counter)
+    vocabulary_fr.build(fr_counter)
 
-    print(len(vocabulary))
+    
+
+
+        
 
 
 
