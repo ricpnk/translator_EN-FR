f90345e22a17fdef1c3b9911dd9868aa24e4b4e0
ricopenkalski <51368644+ricopenkalski@users.noreply.github.com>
2025-05-18 21:29:15 +0200

    added code to test the vocab

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import torch
+import pandas as pd
+from src.Vocab import Vocab
+from collections import Counter
+
 def main():
-    print("Hello from homework-5!")
+    data = pd.read_csv("data/rec05_small_en_fr.csv")
+
+
+    en_counter = Counter()
+    for sentence in data["EN"]:
+        en_counter.update(sentence.split())
+
+    vocabulary = Vocab()
+    vocabulary.build(en_counter)
+
+    print(len(vocabulary))
+
+
 
 
 if __name__ == "__main__":
