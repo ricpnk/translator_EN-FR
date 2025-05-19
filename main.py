a734316a5ebc28ae443dc0e11563670f206d0387
ricopenkalski <51368644+ricopenkalski@users.noreply.github.com>
2025-05-19 03:46:52 +0200

    added structure for models, train-test-split, Hyperparameters

--- main.py
+++ main.py
@@ -2,21 +2,29 @@ import torch
 import pandas as pd
 from collections import Counter
 from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
 
 from src.Vocab import Vocab
 from src.Translation_Data import Translation_Data, collate
+from src.models import Encoder, Decoder, Seq2Seq
 
 
 # Hyperparameters
 BATCH_SIZE = 32
+EMBEDDING_DIM = 256
+HIDDEN_DIM = 512
+MAX_LEN = 15
+LR = 0.001
+
 
 def main():
-    #todo check if apple gpu is available
+    # todo check if apple gpu is available
     device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
     print("Using device: ", device)
 
-    # todo read in the data
+    # todo read in the data and train-test split
     data = pd.read_csv("data/rec05_small_en_fr.csv")
+    train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
 
     # todo initialize counters for the vocabs
     en_counter = Counter()
@@ -33,16 +41,34 @@ def main():
     vocabulary_fr.build(fr_counter)
 
     # todo initiate translator dataset
-    translator_data = Translation_Data(data, vocabulary_en, vocabulary_fr)
+    train_translator_data = Translation_Data(train_data, vocabulary_en, vocabulary_fr)
+    test_translator_data = Translation_Data(test_data, vocabulary_en, vocabulary_fr)
 
     train_loader = DataLoader(
-        translator_data,
+        train_translator_data,
         batch_size=BATCH_SIZE,
         shuffle=True,
         collate_fn=lambda batch: collate(
             batch, special_idx=vocabulary_en.word2idx["<pad>"]
         ),
     )
+    test_loader = DataLoader(
+        test_translator_data,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        collate_fn=lambda batch: collate(
+            batch, special_idx=vocabulary_en.word2idx["<pad>"]
+        ),
+    )
+
+    # todo initiate the Objects for models
+    enc = Encoder()
+
+    dec = Decoder()
+
+    model = Seq2Seq()
+
+
 
 
 if __name__ == "__main__":
