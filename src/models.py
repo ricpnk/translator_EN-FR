12090574389cedc92d54a69b893580bb14ace936
ricopenkalski <51368644+ricopenkalski@users.noreply.github.com>
2025-05-19 03:07:56 +0200

    added basic structure for models

---
+++ src/models.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+class Encoder(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class Decoder(nn.Module):
+
+    def __init___(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class Seq2Seq(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
