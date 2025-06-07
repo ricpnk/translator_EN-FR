64e15064517ebcd5feca757003e1940b4497bfd0
ricopenkalski <51368644+ricopenkalski@users.noreply.github.com>
2025-06-07 03:24:39 +0200

    added new project structure and model saving + checkpoints

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from datetime import datetime
 import torch
 import torch.nn as nn
 import pandas as pd
@@ -5,6 +7,8 @@ from collections import Counter
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 from tqdm.auto import tqdm
+import evaluate
+import matplotlib.pyplot as plt
 
 from src.Vocab import Vocab
 from src.Translation_Data import Translation_Data, collate
@@ -12,20 +16,31 @@ from src.models import Encoder, Decoder, Seq2Seq
 
 
 # Hyperparameters
-BATCH_SIZE = 32
+BATCH_SIZE = 128
 EMBEDDING_DIM = 256
 HIDDEN_DIM = 512
 N_DIM = 1
-MAX_LEN = 15
+MAX_LEN = 12
 LEARNRATE = 0.001
-NUM_EPOCHS = 10
 
+TIMESTAMP = datetime.now().strftime("%Y%m%d_%H%M%S")
+MODEL_PATH = f"saved_models/model_{TIMESTAMP}"
 
 def main():
+    # todo bulid project structure
+    os.makedirs(MODEL_PATH, exist_ok=True)
+    os.makedirs(MODEL_PATH+"/attention_plots", exist_ok=True)
+    os.makedirs(MODEL_PATH+"/checkpoints", exist_ok=True)
     # todo check if apple gpu is available
     device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
     print("Using device: ", device)
 
+    # todo ask if checkpoints should be loaded
+    #! load_checkpoint = input("Do you want to load a checkpoint? (y/n): ")
+    global LOAD_CHECKPOINT
+    LOAD_CHECKPOINT = False
+    #if load_checkpoint == "y":
+    #    LOAD_CHECKPOINT = True
     # todo read in the data and train-test split
     data = pd.read_csv("data/rec05_small_en_fr.csv")
     train_data, test_data = train_test_split(data, test_size=0.1, random_state=42)
@@ -94,6 +109,12 @@ def main():
 def training(model, criterion, optimizer, train_loader, test_loader, device):
     model.train()
     for epoch in range(NUM_EPOCHS):
+    # todo load the checkpoint if it exists
+    if LOAD_CHECKPOINT:
+        checkpoint = torch.load("saved_models/checkpoints/checkpoint.pth")
+        model.load_state_dict(checkpoint["model_state"])
+        optimizer.load_state_dict(checkpoint["optimizer_state"])
+        start_epoch = checkpoint["epoch"] + 1
         epoch_loss = 0
         for batch in tqdm(train_loader, desc=f"Epoch {epoch} [Train]", unit="batch"):
             input = batch["input"].to(device)
@@ -116,12 +137,20 @@ def training(model, criterion, optimizer, train_loader, test_loader, device):
         print(f"Train Loss: {avg_train_loss}")
         print(f"Test Loss: {avg_test_loss}")
 
-def evaluate(model, criterion, epoch, test_loader, device):
+        # save the checkpoint
+        last_checkpoint = {
+            "epoch": epoch,
+            "model_state": model.state_dict(),
+            "optimizer_state": optimizer.state_dict(),
+            "loss": avg_train_loss,
+        }
+        torch.save(last_checkpoint, f"saved_models/model_{TIMESTAMP}/checkpoints/checkpoint.pth")
     model.eval()
     test_loss = 0
 
     with torch.no_grad():
         for batch in tqdm(test_loader, desc=f"Epoch {epoch} [Test]  ", unit="batch"):
+        for batch in tqdm(test_loader, desc=f"Epoch {epoch + 1} [Test]  ", unit="batch"):
             input = batch["input"].to(device)
             output = batch["output"].to(device)
 
